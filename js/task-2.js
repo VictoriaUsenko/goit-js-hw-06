48f90a4b06e34256bf3ecd5b16fbf49916b740a9
Victoria Usenko <usenko.vika@gmail.com>
2024-03-29 23:04:47 +0200

    done

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexItemToRemov = this.#items.indexOf(itemToRemove);
+
+    if (indexItemToRemov !== -1) {
+      this.#items.splice(indexItemToRemov, 1);
+    }
+  }
+}
+
+//////////
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
